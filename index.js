2ecc8fe2e9725a395de11c7920904aa152595dc1
Paul Yang <paulyang00@gmail.com>
2022-09-06 09:56:55 -0400

    commit tree 580fa1f8a0d34a8cd5cc63a5e5b0b4a6de0ffc58

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let thankYouMessages = []
+    for (let i = 0; i < name.length; i++ ) {
+        thankYouMessages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    } 
+    return thankYouMessages
+}
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+        --count;
+    console.log (count)   
+    }
+}
